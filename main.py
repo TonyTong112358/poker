d0cd44112d7cce08eaffbb6f0c5d8c47ff382620
Tony <109382836+peppacaiou@users.noreply.github.com>
2024-02-10 12:44:55 -0800

    fixed font color

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ root = Tk()
 label1 = Label(root, text = "hello world ")
 label1.grid(row = 0 , column = 0 )
  
-label2 = Label(root, text = " world ")
+label2 = Label(root, text = " world ",fg = "red")
 label2.grid(row = 0 , column = 1 )
+
+
 root.mainloop()
 
-print("hello world")
+
